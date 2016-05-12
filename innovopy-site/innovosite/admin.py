1b1df1155a46da9625264042c244b361c362d5e5
rmedinahu <richardmedina@csmb01.nmhu.edu>
2016-05-12 16:50:42 -0600

    setup and config

--- innovopy-site/innovosite/admin.py
+++ innovopy-site/innovosite/admin.py
@@ -2,6 +2,17 @@ from django.contrib import admin
 
 from .models import Innovosite, SubOrganization, Building
 
-admin.site.register(Innovosite)
-admin.site.register(SubOrganization)
-admin.site.register(Building)
+class InnovositeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'url')
+
+class SubOrganizationAdmin(admin.ModelAdmin):
+    list_display = ('name', 'org_site', 'org_type', 'org_parent')
+    list_filter = ('org_site', 'org_type', 'org_parent')
+
+class BuildingAdmin(admin.ModelAdmin):
+    list_display = ('name', 'code', 'building_site')
+    list_filter = ('building_site',)
+
+admin.site.register(Innovosite, InnovositeAdmin)
+admin.site.register(SubOrganization, SubOrganizationAdmin)
+admin.site.register(Building, BuildingAdmin)
